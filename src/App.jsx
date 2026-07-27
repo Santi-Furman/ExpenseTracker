ccf7894eb059bb904d327aed049931f80955ac7b
“Santi <santifurman@gmail.com>
2026-07-27 13:27:43 -0300

    feat: persistencia

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
-
 import Header from "./components/Header/Header";
 import Layout from "./components/Layout/Layout";
 import SummaryCards from "./components/SummaryCards/SummaryCards";
 import ExpenseTable from "./components/ExpenseTable/ExpenseTable";
 
 import defaultExpenses from "./data/defaultExpenses";
+import useLocalStorage from "./hooks/useLocalStorage";
 
 function App() {
 
-  const [expenses, setExpenses] = useState(defaultExpenses);
+  const [expenses, setExpenses] = useLocalStorage(
+    "expenses",
+    defaultExpenses
+  );
 
   return (
     <Layout>
